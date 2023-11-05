7d4ab21d5bb0fdf8682345a468aca513feb57d97
Elmin Midzic <emidza43@gmail.com>
2023-11-05 14:51:41 +0100

    KreiraneKomponente

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Body from './components/body/Body';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
+      <Body/>
+      <Footer />
     </div>
   );
 }
